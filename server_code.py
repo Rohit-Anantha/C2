c877869a9dea55bb59e53891baee129762add017
Rohit Anantha <anantha.rohit.11@gmail.com>
2024-10-26 07:51:13 -0700

    server changes

--- server_code.py
+++ server_code.py
@@ -10,20 +10,22 @@ def server_program():
     host = socket.gethostname()
     port = 1337  # initiate port no above 1024
 
-    server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
-    server_socket.listen(1)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
     
     f = Fernet(key)
 
     while True:
+        server_socket = socket.socket()  # get instance
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((host, port))  # bind host address and port together
+        server_socket.listen(10)
+        print('waiting for connection')
+        conn, address = server_socket.accept()  # accept new connection
+        print("Connection from: " + str(address))
         curr_time = time.localtime()
-        print('retrying connection at: ', curr_time.tm_hour, ':',curr_time.tm_min, ':', curr_time.tm_sec)
+        print('at: ', curr_time.tm_hour, ':',curr_time.tm_min, ':', curr_time.tm_sec)
 
         while True:
             # receive data stream. it won't accept data packet greater than 1024 bytes
@@ -31,25 +33,30 @@ def server_program():
             if not data:
                 # if data is not received break
                 break
-            data = f.decrypt(data)
-            print("from connected user: " + str(data))
-            command = str(data)
+            data = str(f.decrypt(data).decode())
+
+            print(data)
+            print("from connected user: " + data)
+            command = data
             
             # before running commands auth using AES
             # we need a private key
             
-            result = subprocess.run(command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout_output = result.stdout.decode("utf-8")
-            stderr_output = result.stderr.decode("utf-8")
-            res = f.encrypt(stdout_output)
-            conn.send(res.encode())  # send data to the client
-
+            try:
+                result = subprocess.run(command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout_output = result.stdout.decode("utf-8")
+                stderr_output = result.stderr.decode("utf-8")
+                print(stdout_output, stderr_output)
+                res = f.encrypt(stdout_output)
+                conn.send(res.encode())
+            except :
+                print("Something Messed Up")
+              # send data to the client
         conn.close()  # close the connection
+        print('outer loop')
         # wait at least a minute before retrying the connection
-        curr_time.tm_min += 1
-        while curr_time < time.localtime():
-            time.sleep(15)
-            print('current time: ', curr_time.tm_hour, ':',curr_time.tm_min, ':', curr_time.tm_sec)
+        # curr_time.tm_min += 1
+        print('current time: ', curr_time.tm_hour, ':',curr_time.tm_min, ':', curr_time.tm_sec)
             
 
 if __name__ == '__main__':
